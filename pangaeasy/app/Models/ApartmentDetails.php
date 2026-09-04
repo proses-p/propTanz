8b564fc45ecf5a288250c764e1e5f906bdc02a4e
omari joseph <omarirobert98@gmail.com>
2026-09-04 14:45:33 +0300

    repetation

--- pangaeasy/app/Models/ApartmentDetails.php
+++ pangaeasy/app/Models/ApartmentDetails.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ApartmentDetails extends Model
+class ApartmentDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'apartment_details';
+    protected $primaryKey = 'apartment_id';
+
+    protected $fillable = [
+        'name',
+        'description',//hapa ita include street ,town,address
+        'apartment_images_id',
+        
+    ];
+
+    public function images()
+    {
+        return $this->belongsTo(
+            ApartmentImage::class,
+            'apartment_images_id',
+            'images_id'
+        );
+    }
 }
